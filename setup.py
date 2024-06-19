dfb47e17639749f0ff12eeab0a8b6c5e6384ada0
rodfer0x80 <rodrigolf080@gmail.com>
2024-06-19 21:08:21 +0100

    neat code

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name='utils',
-  version='1.0.2',
+  version='1.0.3',
   python_requires='>=3.6, <4.0',
   packages=find_packages(),
   install_requires=[
