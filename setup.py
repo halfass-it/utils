ddf05e697c0f1eb5ad9ad27afb266528dc48648a
rodfer0x80 <rodrigolf080@gmail.com>
2024-06-21 14:28:11 +0100

    Updated to version 0.3.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name='utils',
-  version='0.3.1',
+  version='0.3.2',
   python_requires='>=3.6, <4.0',
   packages=find_packages(),
   install_requires=[
--- utils/packet.py
+++ utils/packet.py
@@ -57,11 +57,9 @@ class CommandPacket(Packet):
   def __post_init__(self):
     self.auth = self.data.get('AUTH', {})
     self.game = self.data.get('GAME', {})
-    self.auth_data = self.auth if self.auth else {}
-    self.game_data = self.game if self.game else {}
 
   def __str__(self) -> str:
-    return str({'AUTH': self.auth_data, 'AUTH': self.game_data})
+    return str({'AUTH': self.auth, 'AUTH': self.game})
 
   def __bytes__(self) -> bytes:
     return str(self).encode('utf-8')
