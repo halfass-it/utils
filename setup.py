0a702b39b8e61122450dae37bffffb61922b665e
rodfer0x80 <rodrigolf080@gmail.com>
2024-06-21 14:18:55 +0100

    Updated to version 0.2.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name='utils',
-  version='0.2.4',
+  version='0.2.5',
   python_requires='>=3.6, <4.0',
   packages=find_packages(),
   install_requires=[
--- utils/types.py
+++ utils/types.py
@@ -8,33 +8,35 @@ import inspect
 import traceback
 from typing import Optional
 
+
 @dataclass
 class Error:
-    msg: str
-    error_type: Optional[str] = None
-    stacktrace: Optional[str] = None
-    error_msg: str = field(init=False)
+  msg: str
+  error_type: Optional[str] = None
+  stacktrace: Optional[str] = None
+  error_msg: str = field(init=False)
 
-    def __post_init__(self):
-        caller_frame = inspect.currentframe().f_back
-        file_path = inspect.getfile(caller_frame)
-        method_name = caller_frame.f_code.co_name
-        line_number = caller_frame.f_lineno
-        if self.error_type is None:
-            self.error_type = "GenericError"
-        if self.stacktrace is None:
-            self.stacktrace = ''.join(traceback.format_stack()[:-1])
-        self.error_msg = (
-            f"[ERROR] {self.error_type}: {self.msg}\n"
-            f"Location: {file_path}:{method_name}:{line_number}\n"
-            f"Stacktrace:\n{self.stacktrace}"
-        )
+  def __post_init__(self):
+    caller_frame = inspect.currentframe().f_back
+    file_path = inspect.getfile(caller_frame)
+    method_name = caller_frame.f_code.co_name
+    line_number = caller_frame.f_lineno
+    if self.error_type is None:
+      self.error_type = 'GenericError'
+    if self.stacktrace is None:
+      self.stacktrace = ''.join(traceback.format_stack()[:-1])
+    self.error_msg = (
+      f'[ERROR] {self.error_type}: {self.msg}\n'
+      f'Location: {file_path}:{method_name}:{line_number}\n'
+      f'Stacktrace:\n{self.stacktrace}'
+    )
 
-    def __str__(self):
-        return self.error_msg
+  def __str__(self):
+    return self.error_msg
 
-    def __repr__(self):
-        return f"Error(msg='{self.msg}', error_type='{self.error_type}')"
+  def __repr__(self):
+    return f"Error(msg='{self.msg}', error_type='{self.error_type}')"
+
 
 @dataclass
 class CurrentDate:
