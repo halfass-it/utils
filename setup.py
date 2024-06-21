7308d4eae76c3a6ed3d55197ea3f46607270f8f3
rodfer0x80 <rodrigolf080@gmail.com>
2024-06-21 17:03:08 +0100

    Updated to version 0.5.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name='utils',
-  version='0.5.6',
+  version='0.5.7',
   python_requires='>=3.6, <4.0',
   packages=find_packages(),
   install_requires=[
