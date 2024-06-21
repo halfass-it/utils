c83366104a071941541f771668fb9939d2d0253b
rodfer0x80 <rodrigolf080@gmail.com>
2024-06-21 14:55:59 +0100

    Updated to version 0.4.3

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,4 +3,9 @@ from .logger import Logger
 from .logger_to_file import LoggerToFile
 from .types import Error, CurrentDate
 from .packet import Packet, CommandPacket
-from .dtypes import AuthPacketStructure, GamePacketStructure, CommandPacketStruct
+from .dtypes import (
+  AuthPacketDataStructure, 
+  GamePacketDataStructure, 
+  CommandPacketDataStructure, 
+  PacketDataStructure
+)
