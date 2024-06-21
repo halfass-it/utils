49a3cf4db6501908e2fca65ab5ae3b26719278b2
rodfer0x80 <rodrigolf080@gmail.com>
2024-06-21 15:29:19 +0100

    Updated to version 0.4.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name='utils',
-  version='0.4.5',
+  version='0.4.6',
   python_requires='>=3.6, <4.0',
   packages=find_packages(),
   install_requires=[
